d06315b5973f88f174b10b3314398ad34e382236
KLisabeth <professional.translations4ppl@gmail.com>
2020-02-12 20:47:06 +0100

    percentage of mentors and students

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalNumber = numberOfStudents + numberOfMentors;
+var percentageOfStudents = Math.round((numberOfStudents/totalNumber) * 100);
+var percentageOfMentors = Math.round((numberOfMentors/totalNumber) * 100);
+
+  
+
+          
+console.log(percentageOfStudents);
+
+console.log(percentageOfMentors);
+
