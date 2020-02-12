d3402e78f3a214a27608e883757ebe463c712959
KLisabeth <professional.translations4ppl@gmail.com>
2020-02-12 21:47:21 +0100

    function parameters

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function addNumbers(passing, parameters) {
+    return passing + parameters;
+    }
 // Call the function and assign to a variable `sum`
+var sum = addNumbers(13, 124)
 
 console.log(sum);
