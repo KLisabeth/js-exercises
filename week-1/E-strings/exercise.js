edc72602f83ce557e224befe9ae49e82f0fa0502
KLisabeth <professional.translations4ppl@gmail.com>
2020-02-12 18:08:42 +0100

    add data type

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,3 +1,14 @@
 // Start by creating a variable `message`
-
+var message = "This is something here";
 console.log(message);
+
+var messageType = typeof message;
+
+console.log(messageType); 
+
+var someNumber = 1983;
+console.log(someNumber);
+
+var messageType = typeof someNumber;
+
+console.log(messageType); 
